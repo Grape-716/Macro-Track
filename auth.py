88acfc074ad196721f7fe1fbfb7c7449adfb8d29
Grape-716 <afzalumar046@gmail.com>
2025-09-11 22:52:00 +0100

    delete push1

--- auth.py
+++
@@ -1,9 +0,0 @@
-
-btn = ctk.CTkButton(master=app, text="Install ", 
-                    hover_color="#57615F",  
-                    font=("Segoe UI", 13, "bold"),
-                    corner_radius=32, fg_color="transparent",
-                    border_color="#FFFFFF", 
-                    border_width=1.5, )
-
-btn.place(relx=0.48, rely=0.5, anchor="center")
